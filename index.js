6d2a1ab0c659316885875982440991d16e17aa2f
Caleb-MUGISHA <c.mugisha@alustudent>
2021-12-10 20:34:25 +0200

    organisation

--- index.js
+++ index.js
@@ -2,20 +2,23 @@ import express from "express";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import router from "./routers/index.js"
-const app=express();
+const app = express();
 
 dotenv.config()
-const port=process.env.PORT;
+const port = process.env.PORT;
 
+app.configure(function() {
+    app.use(express.static(path.join(__dirname, 'public')));
+});
 
-app.get("/",(req,res)=>{
-    res.send("Hello World")
+app.get("/", (req, res) => {
+    res.render('./index.html')
 })
 
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(router);
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Our HealthHub API is now available on port${port}`)
 })
