653a340f5df8af40b55f118e1141584b6cf680ae
Caleb-MUGISHA <c.mugisha@alustudent>
2021-12-11 17:52:08 +0200

    user-route

--- index.js
+++ index.js
@@ -5,40 +5,40 @@ import router from "./routers/index.js";
 import cors from "cors";
 
 
-const app=express();
+const app = express();
 
 dotenv.config()
-const port=process.env.PORT;
+const port = process.env.PORT;
 
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.send("Hello World")
 })
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(router);
 
- app.get('/patient/:id', function (req, res, next) {
-     res.json({ msg: 'This is CORS-enabled for all origins!' })
- })
-app.get('/adminRouter/:id', function (req, res, next) {
+app.get('/patient/:id', function(req, res, next) {
     res.json({ msg: 'This is CORS-enabled for all origins!' })
 })
-app.get('/hospital/:id', function (req, res, next) {
+app.get('/adminRouter/:id', function(req, res, next) {
     res.json({ msg: 'This is CORS-enabled for all origins!' })
 })
-app.get('/appointment/:id', function (req, res, next) {
+app.get('/hospital/:id', function(req, res, next) {
     res.json({ msg: 'This is CORS-enabled for all origins!' })
 })
-//app.get('/router/:id', function (req, res, next) {
- //   res.json({ msg: 'This is CORS-enabled for all origins!' })
-//})
+app.get('/appointment/:id', function(req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' })
+})
+app.get('/user/:id', function(req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' })
+})
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     console.log(`Our HealthHub API is now available on port${port}`)
 })
