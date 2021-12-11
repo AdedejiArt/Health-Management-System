f202fd94929543ed7fbd739da2747afa93704892
AdedejiArt <a.roheem@alustudent.com>
2021-12-10 23:22:40 -0800

    Added a new File

--- index.js
+++ index.js
@@ -12,9 +12,29 @@ app.get("/",(req,res)=>{
     res.send("Hello World")
 })
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use(router);
+
+ app.get('/patient/:id', function (req, res, next) {
+     res.json({ msg: 'This is CORS-enabled for all origins!' })
+ })
+app.get('/adminRouter/:id', function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' })
+})
+app.get('/hospital/:id', function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' })
+})
+app.get('/appointment/:id', function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' })
+})
+//app.get('/router/:id', function (req, res, next) {
+ //   res.json({ msg: 'This is CORS-enabled for all origins!' })
+//})
+
+
 
 app.listen(port,()=>{
     console.log(`Our HealthHub API is now available on port${port}`)
