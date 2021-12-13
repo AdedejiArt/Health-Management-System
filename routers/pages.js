1053c316e1a64b7a9e02f3860c6efd38ae299755
Caleb-MUGISHA <c.mugisha@alustdent.com>
2021-12-13 23:08:54 +0200

    patient

--- routers/pages.js
+++ routers/pages.js
@@ -1,15 +1,33 @@
 import express from "express";
 
-const pagesrouter= express.Router();
+const pagesrouter = express.Router();
 
-pagesrouter.get('/',(req,res)=>{
- res.render ('index');
+pagesrouter.get('/', (req, res) => {
+    res.render('index');
 })
-pagesrouter.get('/register',(req,res)=>{
+pagesrouter.get('/register', (req, res) => {
     res.render('register');
 })
-pagesrouter.get('/login',(req,res)=>{
+pagesrouter.get('/login', (req, res) => {
     res.render("login")
+})
+pagesrouter.get('/doctor-profile', (req, res) => {
+    res.render("doctor-profile")
+})
+pagesrouter.get('/booking', (req, res) => {
+    res.render("booking")
+})
+pagesrouter.get('/appointments', (req, res) => {
+    res.render("appointments")
+})
+pagesrouter.get('/doctor-register', (req, res) => {
+    res.render("doctor-register")
+})
+pagesrouter.get('/doctor-dashboard', (req, res) => {
+    res.render("doctor-dashboard")
+})
+pagesrouter.get('/checkout', (req, res) => {
+    res.render("checkout")
 })
 
 export default pagesrouter;
